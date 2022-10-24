e2506045df0fac8ceeef0f9ea6baafc79d3ab37b
Stephanie Gagnon <gag.stephanie@gmail.com>
2022-10-23 20:46:30 -0400

    ajout date + offline computers

--- devoir1.ps1
+++ devoir1.ps1
@@ -5,10 +5,10 @@ function Get-FormattedFileSize
     [CmdletBinding()]
 	param(
         [Parameter(Mandatory=$true,Position=0)]
-        [ulong] $FileSize
+        [ulong] $fileSize
     )
 
-    switch($FileSize) {
+    switch($fileSize) {
         {$_ -gt 1TB }
             { "{0:n2} TB" -f ($_ / 1TB); break }
         {$_ -gt 1GB }
@@ -22,64 +22,65 @@ function Get-FormattedFileSize
     }
 }
 
-# Définitions pour le PowerShell à distance
+# Obtenir les ordinateurs actifs dans Active Directory
 
-$VMNames = Get-ADComputer -Filter * -Properties Name,LastLogonDate | Where-Object {$_.Enabled} | Select-Object -ExpandProperty Name
+$vmNames = Get-ADComputer -Filter * -Properties Name,LastLogonDate | Where-Object {$_.Enabled} | Select-Object -ExpandProperty Name
+$offlineVMs = @()
 
 # Création du rapport
 
-$ReportSections = @()
+$reportSections = @()
 
-ForEach ($VMName in $VMNames) {
-    $VMSession = New-PSSession $VMName
+ForEach ($vmName in $vmNames) {
     
-    # Obtenir le nom de la machine
-    $NameInfo = Invoke-Command -ScriptBlock {
-        [PSCustomObject]@{
-            ComputerName = $env:computername
-        }
-    } -Session $VMSession
+    $vmSession = New-PSSession $vmName -ErrorAction SilentlyContinue
 
-    $NameInfoHTML = "<h1>Computer name: $($NameInfo.ComputerName)</h1>"
-    $ReportSections += $NameInfoHTML
+    if ($null -eq $vmSession) {
+        Write-Host "$vmName is offline."
+        $offlineVMs += $vmName
+        Continue;
+    }
 
+    $reportSections += "<h1>Computer name: $vmName</h1>"
+    
     # Obtenir les informations du système d'exploitation, convertir le résultat en code HTML dans une table et le stocker dans une variable
 
-    $OSinfo = Invoke-Command -ScriptBlock {
+    $osInfo = Invoke-Command -ScriptBlock {
         Get-CimInstance -ClassName Win32_OperatingSystem
-    } -Session $VMSession
+    } -Session $vmSession
 
-    $UptimeFilter = @{Label='Uptime';Expression={((Get-Date) - $_.LastBootUpTime).ToString("hh\:mm\:ss")}}
-    $OSinfoHTML = $OSinfo | ConvertTo-Html -Property Version,Caption,BuildNumber,$UptimeFilter -Fragment -PreContent "<h2>Operating System Information</h2>"
-    $ReportSections += $OSinfoHTML
+    $uptimeFilter = @{Label='Uptime';Expression={((Get-Date) - $_.LastBootUpTime).ToString("hh\:mm\:ss")}}
+    $osInfoHTML = $osInfo | ConvertTo-Html -Property Version,Caption,BuildNumber,$upTimeFilter -Fragment -PreContent "<h2>Operating System Information</h2>"
+    $reportSections += $osInfoHTML
 
     # Obtenir les informations du processeur, convertir le résultat en code HTML dans une table et le stocker dans une variable
 
-    $ProcessInfo = Invoke-Command -ScriptBlock {
+    $processInfo = Invoke-Command -ScriptBlock {
         Get-CimInstance -ClassName Win32_Processor
-    } -Session $VMSession
+    } -Session $vmSession
 
-    $ProcessInfoHTML = $ProcessInfo | ConvertTo-Html -Property DeviceID,Name,Caption -Fragment -PreContent "<h2>Processor Information</h2>"
-    $ReportSections += $ProcessInfoHTML
+    $processInfoHTML = $processInfo | ConvertTo-Html -Property DeviceID,Name,Caption -Fragment -PreContent "<h2>Processor Information</h2>"
+    $reportSections += $processInfoHTML
 
     # Obtenir les informations du disque, appeler la fonction Get-FormattedFileSize, convertir le résultat en code HTML dans une table et le stocker dans une variable
 
-    $DiskInfo = Invoke-Command -ScriptBlock {
+    $diskInfo = Invoke-Command -ScriptBlock {
         Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3"
-    } -Session $VMSession
+    } -Session $vmSession
 
-    $SizeFilter = @{Label='Size';Expression={Get-FormattedFileSize($_.Size)}}
-    $FreeSpaceFilter = @{Label='FreeSpace';Expression={Get-FormattedFileSize($_.FreeSpace)}}
-    $DiskInfo = $DiskInfo | Select-Object -Property DeviceID,DriveType,$SizeFilter,$FreeSpaceFilter
-    $DiskInfoHTML = $DiskInfo | ConvertTo-Html -Property DeviceID,DriveType,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
-    $ReportSections += $DiskInfoHTML
+    $sizeFilter = @{Label='Size';Expression={Get-FormattedFileSize($_.Size)}}
+    $freeSpaceFilter = @{Label='FreeSpace';Expression={Get-FormattedFileSize($_.FreeSpace)}}
+    $diskInfo = $diskInfo | Select-Object -Property DeviceID,DriveType,$sizeFilter,$freeSpaceFilter
+    $diskInfoHTML = $diskInfo | ConvertTo-Html -Property DeviceID,DriveType,Size,FreeSpace -Fragment -PreContent "<h2>Disk Information</h2>"
+    $reportSections += $diskInfoHTML
 }
 
 # Toutes les informations sont rassemblées dans un seul rapport HTML, incluant la date de création du rapport
 
-$ReportBody = $ReportSections | Join-String -Separator " "
-$Report = ConvertTo-HTML -Body $ReportBody -Title "Computer Information Report" -PostContent "<p><b>Date:<b> $((Get-Date).ToString("dd-MM-yyyy"))<p>"
+$reportSections += "<h2>Offline Computers: $($offlineVMs | Join-String -Separator ", ")</h2>"
+$reportBody = $reportSections | Join-String -Separator " "
+$report = ConvertTo-HTML -Body $reportBody -Title "Computer Information Report" -PostContent "<p><b>Date:<b> $((Get-Date).ToString("dd-MM-yyyy"))<p>"
 
 # Générer le rapport en un fichier HTML
-
-$Report | Out-File .\ComputerReport.html
+$reportName = "ComputerReport_" + (Get-Date -Format "ddMMyyyyy") + ".html"
+$report | Out-File .\$reportName
